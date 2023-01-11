2804c415d97be46b19e28c05a40d9799a6f99dd8
Vinicius Silva <viniciusvinisilva1@outlook.com>
2023-01-10 22:56:06 -0300

    chore: add jest configs and scripts

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -32,7 +32,8 @@ export class SignUpController implements Controller {
         password
       })
       return ok(account)
-    } catch {
+    } catch (error) {
+      console.error(error)
       return serverError()
     }
   }
